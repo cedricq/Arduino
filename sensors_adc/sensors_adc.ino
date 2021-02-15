57a5811e1d52fddd6c923efc1af8206dc2386f46
Cedric Quentin <cedric.quentin@wandercraft.eu>
2021-02-15 20:51:24 +0100

    adding honeywell pressure adc sensor

---
+++ sensors_adc/sensors_adc.ino
@@ -0,0 +1,58 @@
+int flowSensorPin = A0;    
+int pressureSensorPin = A1;    
+
+#define P_MIN     0.0      
+#define P_MAX     68.9476  // 1 psi in mbar
+#define RAW_MAX   1023     // ADC 10 bits : 2^10 - 1
+
+bool led_state = true;
+int ledPin = 13;           // select the pin for the LED
+
+float sensirion_convertToLMin(int raw)
+{
+  return (212.5F * ( ((float)raw / 5.0F) - 1 ) ) - 10 ;
+}
+
+
+float honeywell_convertToPressure(unsigned int raw, float pmin, float pmax, int raw_max)
+{
+  return ( ( pmax - pmin ) * ( raw - 0.1f * raw_max ) / ( 0.8f * raw_max ) ) + pmin ;
+}
+
+void toggle_pin(int pin_id){
+  if (digitalRead(pin_id) == HIGH  )
+    digitalWrite(ledPin, LOW);
+  else
+    digitalWrite(ledPin, HIGH);  
+} 
+
+void setup() 
+{
+  Serial.begin(9600);
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() 
+{ 
+  int rawFlow = analogRead(flowSensorPin) ;
+  float flowLmin = sensirion_convertToLMin(rawFlow);
+
+  int rawPressure = analogRead(pressureSensorPin) ;
+  float pressureMbar = honeywell_convertToPressure(rawPressure, P_MIN, P_MAX, RAW_MAX);
+
+  Serial.print("Raw flow : "); 
+  Serial.print(rawFlow); 
+  Serial.print("\tFlow (L/min) : "); 
+  Serial.print(flowLmin);
+
+  Serial.print("\t\tRaw pressure : "); 
+  Serial.print(rawPressure); 
+  Serial.print("\tPressure (mbar) : "); 
+  Serial.println(pressureMbar);
+  
+  // turn the ledPin on
+  toggle_pin(ledPin);
+  
+  delay(100);
+}
